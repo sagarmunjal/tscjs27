67bd2fa9812c040292d116d225fe587470e3c5f4
Sagar <sagar@Sagars-MacBook-Air.local>
2018-12-26 13:33:57 +0530

    Practice yourself - write basic mathematical operation functions product, subtract, divide.

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('the script file has successfully loaded'); 
+// Writing basic functions in Javascript. 
+// However, before we continue let's understand few basic concepts. 
+
+
+// Let's begin writing functions which involve basic arithmatic operations like Addition, Subtraction, Division, Multiplication. 
+
+
+function sum (a,b){
+    return a+b;
+}
+
+console.log(`The sum of 4 & 5 is ${sum(4,5)}`);
+
+// Practice yourself 
+// add the function body and log the results in next line seeing the above example. 
+
+function product(a,b){
+
+}
+
+function sub (a,b){
+
+}
+
+function div(a,b) {
+
+}
